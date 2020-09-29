0ffb06bd5b1f5eaa306bdba69910d7b5eff0c6fd
Wagner Martins Rosa <wagnermartins410@gmail.com>
2020-09-28 21:10:57 -0300

    Troco ok

--- src/at_algoritmos/program/TrocoMain.java
+++ src/at_algoritmos/program/TrocoMain.java
@@ -1,6 +1,8 @@
 package at_algoritmos.program;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Scanner;
 
 import at_algoritmos.troco.TrocoDinamico;
 import at_algoritmos.troco.TrocoGulosoRecursivo;
@@ -8,70 +10,119 @@ import at_algoritmos.troco.TrocoGulosoRecursivo;
 public class TrocoMain {
 
 	public static void main(String[] args) {
-		
-//		LinkedHashMap<Double, Integer> granaHash = new LinkedHashMap<Double, Integer>() {
-//			private static final long serialVersionUID = 1L;
-//
-//		{
-//			put(0.01, 0);
-//			put(0.05, 0);
-//			put(0.10, 0);
-//			put(0.25, 0);
-//			put(0.50, 0);
-//			put(1.00, 0);
-//			put(2.00, 0);
-//			put(5.00, 0);
-//			put(10.00, 0);
-//			put(20.00, 0);
-//			put(50.00, 0);
-//			put(100.00, 0);			
-//		}};
-// 
-//		double[] grana = {0.01, 0.05, 0.10, 0.25, 0.50,1.00, 2.00, 5.00, 10.00, 20.00, 50.00, 100.00};
-//		
-//		Double quantia = 188.91;	
-//		
-//		TrocoGulosoRecursivo.fazerTrocoHash(granaHash, quantia);
-//		System.out.println("Troco recursivo de " + quantia + ": ");
-//		
-//		
-//		Double soma = 0.0;
-//		for (Double k : granaHash.keySet()) {
-//			if(k < 1.0) {
-//				System.out.println(k + ": " + granaHash.get(k) + " Moeda(s)");
-//			}else {
-//				System.out.println(k + ": " + granaHash.get(k) + " Nota(s)");
-//			}
-//			soma += Math.round((granaHash.get(k) * k)*100.0)/100.0;
-//		}
-//		
-//		System.out.println();
-//		System.out.println("Soma das moedas e notas: " + soma);
-		
-		int[] moedas = {1,5,6,9};
-		
-		int valor = 10;
-		
-		int[][] tabela = TrocoDinamico.fazerTrocoDinamico(moedas, valor);
-		
-		System.err.println("  0 1 2 3 4 5 6 7 8 9 10");
-		for (int i = 0; i < tabela.length; i++) {
-			System.err.print(moedas[i] + " ");
-			for (int j = 0; j < tabela[i].length; j++) {								
-					System.out.print(tabela[i][j] + " ");
-				
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Escolha qual rodar:\n1)Recursivo/Guloso\n2)Dinâmico\n3)Sair");
+
+		int escolha = sc.nextInt();
+		while (escolha != 3) {
+			switch (escolha) {
+			case 1:
+				rodarGuloso();
+				break;
+			case 2:
+				rodarDinamico();
+				break;
+
+			default:
+				break;
 			}
-			System.out.println();
-		}	
+			System.out.println("Escolha qual rodar:\n1)Recursivo/Guloso\n2)Dinâmico\n3)Sair");
+			escolha = sc.nextInt();
+		}
+
+	}
+
+	private static void rodarGuloso() {
+		LinkedHashMap<Double, Integer> granaHash = new LinkedHashMap<Double, Integer>() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				put(0.01, 0);
+				put(0.05, 0);
+				put(0.10, 0);
+				put(0.25, 0);
+				put(0.50, 0);
+				put(1.00, 0);
+				put(2.00, 0);
+				put(5.00, 0);
+				put(10.00, 0);
+				put(20.00, 0);
+				put(50.00, 0);
+				put(100.00, 0);
+			}
+		};
+
+		double[] grana = { 0.01, 0.05, 0.10, 0.25, 0.50, 1.00, 2.00, 5.00, 10.00, 20.00, 50.00, 100.00 };
+
+		Double quantia = 188.91;
+
+		TrocoGulosoRecursivo.fazerTrocoHash(granaHash, quantia);
+		System.out.println("Troco recursivo de " + quantia + ": ");
+
+		Double soma = 0.0;
+		for (Double k : granaHash.keySet()) {
+			if (k < 1.0) {
+				System.out.println(k + ": " + granaHash.get(k) + " Moeda(s)");
+			} else {
+				System.out.println(k + ": " + granaHash.get(k) + " Nota(s)");
+			}
+			soma += Math.round((granaHash.get(k) * k) * 100.0) / 100.0;
+		}
+
+		System.out.println();
+		System.out.println("Soma das moedas e notas: " + soma);
+
+	}
+
+	private static void rodarDinamico() {
+		int[] moedas = { 1, 5, 10, 25, 50, 100, 200, 500, 1000, 2000, 5000, 10000 };
+		int[] moedasAT = { 1, 5, 10, 25, 21 };
+
+		int valor = 18891;
+		int valorAT = 63;
+
+		int[][] tabela = TrocoDinamico.fazerTrocoDinamico(moedas, valor);
+		int[][] tabelaAT = TrocoDinamico.fazerTrocoDinamico(moedasAT, valorAT);
+
+		System.out.println();
+
+		System.out.print("Número mínimo de moedas: ");
+		System.out.println(TrocoDinamico.numeroDeMoedas(tabela));
+
+		ArrayList<Integer> moedasUsadas = TrocoDinamico.moedasUsadas(tabela, moedas, valor);
+
+		System.out.println(TrocoDinamico.imprimirMoedas(moedas, moedasUsadas));
 		
 		System.out.println();
 		
-		System.out.println(tabela[moedas.length-1][valor]);
-		
-		System.out.println(tabela[tabela.length-1][tabela[tabela.length-1].length-1]);
-		
-		System.out.println(TrocoDinamico.moedasUsadas(tabela, moedas, valor));
+		System.out.println(imprimirTabela(valorAT, tabelaAT, moedasAT));
 
+		System.out.print("Número mínimo de moedasdo AT: ");
+		System.out.println(TrocoDinamico.numeroDeMoedas(tabelaAT));
+
+		ArrayList<Integer> moedasUsadasAT = TrocoDinamico.moedasUsadas(tabelaAT, moedasAT, valorAT);
+
+		System.out.println(TrocoDinamico.imprimirMoedas(moedasAT, moedasUsadasAT));
+	}
+
+	private static String imprimirTabela(int valor, int[][] tabela, int[] moedas) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("  ");
+		for (int i = 0; i < valor + 1; i++) {
+			sb.append(i + " ");
+		}
+		sb.append("\n");
+		for (int i = 0; i < tabela.length; i++) {
+			sb.append(moedas[i]).append(" ");
+			for (int j = 0; j < tabela[i].length; j++) {
+				sb.append(tabela[i][j]).append(" ");
+
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 }
--- src/at_algoritmos/troco/TrocoDinamico.java
+++ src/at_algoritmos/troco/TrocoDinamico.java
@@ -1,47 +1,70 @@
 package at_algoritmos.troco;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class TrocoDinamico {
-	
+
 	public static int[][] fazerTrocoDinamico(int[] grana, int quantia) {
-		
-		int[][] tabela = new int[grana.length][quantia+1];		
-		
+
+		int[][] tabela = new int[grana.length][quantia + 1];
+
 		for (int i = 0; i < tabela.length; i++) {
-			for(int j = 0; j <= quantia; j++) {
+			for (int j = 0; j <= quantia; j++) {
 				if (i == 0) {
-					tabela[i][j] = j/grana[i];
-				}
-				else if(grana[i] > j) {
-					tabela[i][j] = tabela[i-1][j];					
+					tabela[i][j] = j / grana[i];
+				} else if (grana[i] > j) {
+					tabela[i][j] = tabela[i - 1][j];
 				} else {
-					tabela[i][j] = Math.min(tabela[i-1][j], (1 + tabela[i][j - grana[i]]));
+					tabela[i][j] = Math.min(tabela[i - 1][j], (1 + tabela[i][j - grana[i]]));
 				}
 			}
-		}			
-		
+		}
+
 		return tabela;
 	}
-	
+
+	public static int numeroDeMoedas(int[][] tabela) {
+		return tabela[tabela.length - 1][tabela[tabela.length - 1].length - 1];
+	}
+
 	public static ArrayList<Integer> moedasUsadas(int[][] tabela, int[] grana, int quantia) {
 		ArrayList<Integer> moedas = new ArrayList<Integer>();
-		int count = 1;
-		int linha = tabela.length-1;
-		int coluna = tabela[tabela.length-1].length-1;
-		int moeda = 0;
-		
-		
-		for (int i = linha; linha >= 0; i--) {
-			
-			if (tabela[i] )
-			
+		int linha = tabela.length - 1;
+		int coluna = tabela[tabela.length - 1].length - 1;
+
+		for (int i = linha - 1; i >= 0; i--) {
+			while (tabela[i][coluna] != tabela[i + 1][coluna]) {
+				moedas.add(grana[i + 1]);
+				coluna = coluna - grana[i + 1];
+			}
 		}
-	
-		
+
+		while (coluna < grana[1] && coluna > 0) {
+			moedas.add(grana[0]);
+			coluna -= grana[0];
+		}
 		return moedas;
-		
-		
+
+	}
+
+	public static String imprimirMoedas(int[] grana, ArrayList<Integer> moedasUsadas) {
+		StringBuilder sb = new StringBuilder();
+		LinkedHashMap<Integer, Integer> moedas = new LinkedHashMap<>();
+		for (int m : grana) {
+			moedas.put(m, 0);
+		}
+		for (Integer m : moedasUsadas) {
+			for (Integer k : moedas.keySet()) {
+				if (k.equals(m)) {					
+					moedas.put(k, moedas.get(k) + 1);
+				}
+			}
+		}
+		for (Integer k : moedas.keySet()) {			
+			sb.append(String.format("%.2f", (double) k/100.0 )).append(": ").append(String.valueOf(moedas.get(k))).append("\n");
+		}
+		return sb.toString();
 	}
 
 }
